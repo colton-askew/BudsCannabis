6f36398ee9bf80b7d86d158d83c3fbe16a75a9ac
colton-askew <35784323+colton-askew@users.noreply.github.com>
2021-08-31 21:35:07 -0700

    Update Products.js

--- budscannabis/src/Products.js
+++ budscannabis/src/Products.js
@@ -5,7 +5,7 @@ import './Components/menu/ProductCatalog.css';
 function Products(props) {
     return (
         <div>
-            <h1>Welcome to the Products colby
+            <h1>WEED
             </h1>
             <main className = "spectrum-grid">
                 <div id = "product-catalog">
